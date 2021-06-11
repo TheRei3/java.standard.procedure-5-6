50c8cf6e4b5d8919d28cfa2f2d4741205f6b0795
admin <admin@DESKTOP-8L2IG92>
2021-06-12 07:13:08 +0900

    ex5no6

---
+++ ex5no6/src/ex5no6/ex5no6.java
@@ -0,0 +1,23 @@
+package ex5no6;
+
+public class ex5no6 {  // 197p
+	public static void main(String[] args) {
+		int[] score = { 79, 88, 91, 33, 100, 55, 95 } ;
+		
+		int max = score[0] ;
+		int min = score[0] ;
+		
+		for( int i = 1 ; i < score.length ; i++ ) {
+			if( score[i] > max ) {
+				max = score[i] ;
+			}else if( score[i] < min ){
+				min = score[i] ;
+			}
+		}  // for
+		
+		System.out.println("max : " + max);
+		System.out.println("min : " + min);
+		
+	} // main
+
+}
